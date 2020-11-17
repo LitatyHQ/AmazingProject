5fede1502fcedbd260336f861d079ceee24bb520
LitatyHQ <litatyHQ@gmail.com>
2020-11-17 18:07:21 -0500

    first commit

---
+++ lib/amazingproject.rb
@@ -0,0 +1,6 @@
+require "amazingproject/version"
+
+module Amazingproject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/amazingproject_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Amazingproject do
+  it "has a version number" do
+    expect(Amazingproject::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
